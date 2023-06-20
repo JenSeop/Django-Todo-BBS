727a45f575ecb6a17177ee6b3c79ad99c6ada25f
JenSeop <dostwoman@gmail.com>
2023-06-20 20:34:05 +0900

    [23.06.20] upload detail view module

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.todo_list, name='todo_list'),
+    path('<int:pk>/', views.todo_detail, name='todo_detail'),
 ]
