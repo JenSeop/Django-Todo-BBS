e70fcc29144a12e1a4d2a83e26fdba8776dd55f7
JenSeop <dostwoman@gmail.com>
2023-06-20 19:14:38 +0900

    [23.06.20] upload Todo Model & Make Admin page

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100) # Todo의 제목
+    description = models.TextField(blank=True) # Todo에 대한 설명
+    created = models.DateTimeField(auto_now_add=True) # Todo 생성 일자
+    complete = models.BooleanField(default=False) # Todo 완료 여부
+    important = models.BooleanField(default=False) # Todo 중요 여부
+
+    def __str__(self):
+        return self.title
